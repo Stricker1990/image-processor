784b858d46cf5bc245d231f7c598fc6705d55f41
Kirill Pavlov <stricker1990@gmail.com>
2024-12-25 00:04:13 +0400

    configure docker images

--- ImageProcessor/Program.cs
+++ ImageProcessor/Program.cs
@@ -1,8 +1,12 @@
 using ImageProcessor.Domain.Interfaces;
 using ImageProcessor.Services;
 using Microsoft.Extensions.Azure;
+using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri")!);
+builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
 
 builder.Services.AddCors(options =>
 {
